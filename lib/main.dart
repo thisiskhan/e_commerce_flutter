8098e961f028f5ef1cccedb8e807b22916387a38
Mohammed Raza Khan <72858063+Razakhan4772@users.noreply.github.com>
2020-10-22 21:15:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,78 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-
+import 'package:e_commerce_flutter/Frontend/frontend.dart';
+import 'dart:math';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+      routes: {'Login': (context) => Login()},
     );
+
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-
-  final String title;
-
+class SplashScreen extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
+class _SplashScreenState extends State<SplashScreen> {
+  void startTimer() {
+    Timer(Duration(seconds: 1), () {
+      Navigator.of(context).pushReplacementNamed('Login');
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
+        backgroundColor: Colors.white,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.purple,
+                  Colors.teal,
 
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+                ]
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+          ),
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                  child:
+                  Text(
+                    "e-commerce",
+                    style: TextStyle(
+                       // fontFamily: "Satisfy",
+                        fontSize: 50.0,
+                        color: Colors.white
+                    ),
+                  )
+
+              ),
+            ],
+          ),
+
+        )
     );
   }
 }
